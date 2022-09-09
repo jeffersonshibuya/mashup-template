8e88bfaf4e70bb45923304725bde1bfc845cf839
jefferson shibuya <Jefferson Shibuya>
2022-09-08 21:47:50 -0300

    initial configuration

---
+++ src/App.test.tsx
@@ -0,0 +1,31 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { App, WrappedApp } from './App';
+
+describe('App', () => {
+  it('renders hello world!', () => {
+    render(<WrappedApp />);
+
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello World');
+  });
+
+  it('renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Not Found');
+  });
+});
