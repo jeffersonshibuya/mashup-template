92b708f7b954bc64c9893900392ccc82e3e44c29
jefferson shibuya <Jefferson Shibuya>
2022-09-12 14:41:26 -0300

    Added and configured axios connecting to api gateway - mashup-api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,29 @@
+import { useEffect } from "react";
+import { api } from "../services/api";
+
+type mashupConfigData = {
+  host: string;
+  name: string;
+  sheets: {
+    sheetId: string;
+    title: string;
+  }[]
+}
+
 function Home() {
+
+  useEffect(() => {
+    api.post<mashupConfigData>('', 
+    {
+      name: 'nfl',
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(response => console.log(response.data))
+  }, [])
+
   return <h1 className="text-3xl font-bold underline">Hello World</h1>;
 }
 
